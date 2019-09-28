b2806bef3134584b9527a85615b36adac4b9fe6b
zafranf <zafranfadilah@gmail.com>
2019-09-28 20:04:25 +0700

    checklist item test scripts

---
+++ tests/ItemTest.php
@@ -0,0 +1,234 @@
+<?php
+
+class ItemTest extends TestCase
+{
+    protected $faker;
+    protected $checklist;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->faker = \Faker\Factory::create();
+        $this->checklist = \App\Checklist::orderBy('created_at', 'desc')->first();
+    }
+
+    /**
+     * Test List All Items
+     *
+     * @return void
+     */
+    public function testListAllItems()
+    {
+        /* get user */
+        $user = \App\User::find(1);
+
+        /* send request */
+        $this->actingAs($user)->get('/api/checklists/' . $this->checklist->id . '/items');
+
+        /* check status code */
+        $this->seeStatusCode(200);
+
+        /* check response structure */
+        $this->seeJsonStructure([
+            'current_page',
+            'data' => [
+                'id',
+                'description',
+                'due',
+                'urgency',
+                'task_id',
+                'object_id',
+                'object_domain',
+                'is_completed',
+                'completed_at',
+                'updated_by',
+                'created_at',
+                'updated_at',
+                'items' => [
+                    '*' => [
+                        'id',
+                        'description',
+                        'is_completed',
+                        'completed_at',
+                        'due',
+                        'urgency',
+                        'checklist_id',
+                        'created_by',
+                        'updated_by',
+                        'created_at',
+                        'updated_at',
+                    ],
+                ],
+            ],
+            'first_page_url',
+            'last_page_url',
+            'next_page_url',
+            'prev_page_url',
+            'total',
+        ]);
+    }
+
+    /**
+     * Test Get Single Item
+     *
+     * @return void
+     */
+    public function testGetItem()
+    {
+        /* get user */
+        $user = \App\User::find(1);
+
+        /* get item */
+        $item = \App\ChecklistItem::orderBy('created_at', 'desc')->first();
+
+        /* send request */
+        $this->actingAs($user)->get('/api/checklists/' . $this->checklist->id . '/items/' . $item->id);
+
+        /* check status code */
+        $this->seeStatusCode(200);
+
+        /* check response structure */
+        $this->seeJsonStructure([
+            'success',
+            'message',
+            'data' => [
+                'id',
+                'description',
+                'due',
+                'urgency',
+                'task_id',
+                'object_id',
+                'object_domain',
+                'is_completed',
+                'completed_at',
+                'updated_by',
+                'created_at',
+                'updated_at',
+                'items' => [
+                    'id',
+                    'description',
+                    'is_completed',
+                    'completed_at',
+                    'due',
+                    'urgency',
+                    'checklist_id',
+                    'created_by',
+                    'updated_by',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * Test Create a New Item
+     *
+     * @return void
+     */
+    public function testCreateItem()
+    {
+        /* get user */
+        $user = \App\User::find(1);
+
+        /* send request */
+        $this->actingAs($user)->json('post', '/api/checklists/' . $this->checklist->id . '/items', [
+            'data' => [
+                'description' => $this->faker->sentence(rand(3, 6)),
+                'due' => \Carbon\Carbon::now(),
+                'urgency' => rand(1, 5),
+                'checklist_id' => $this->checklist->id,
+            ],
+        ]);
+
+        /* check status code */
+        $this->seeStatusCode(201);
+
+        /* check response structure */
+        $this->seeJsonStructure([
+            'success',
+            'message',
+            'data' => [
+                'id',
+                'description',
+                'is_completed',
+                'completed_at',
+                'due',
+                'urgency',
+                'checklist_id',
+                'created_by',
+                'updated_by',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+
+    /**
+     * Test Update Item
+     *
+     * @return void
+     */
+    public function testUpdateItem()
+    {
+        /* get user */
+        $user = \App\User::find(1);
+
+        /* get item */
+        $item = \App\ChecklistItem::orderBy('created_at', 'desc')->first();
+
+        /* send request */
+        $is_complete = rand(0, 1);
+        $this->actingAs($user)->json('patch', '/api/checklists/' . $this->checklist->id . '/items/' . $item->id, [
+            'data' => [
+                'description' => $this->faker->sentence(rand(3, 6)),
+                'due' => \Carbon\Carbon::now(),
+                'urgency' => rand(1, 5),
+                'checklist_id' => $this->checklist->id,
+            ],
+        ]);
+
+        /* check status code */
+        $this->seeStatusCode(201);
+
+        /* check response structure */
+        $this->seeJsonStructure([
+            'success',
+            'message',
+            'data' => [
+                'id',
+                'description',
+                'is_completed',
+                'completed_at',
+                'due',
+                'urgency',
+                'checklist_id',
+                'created_by',
+                'updated_by',
+                'created_at',
+                'updated_at',
+            ],
+        ]);
+    }
+
+    /**
+     * Test Delete Item
+     *
+     * @return void
+     */
+    public function testDeleteItem()
+    {
+        /* get user */
+        $user = \App\User::find(1);
+
+        /* get item */
+        $item = \App\ChecklistItem::orderBy('created_at', 'desc')->first();
+
+        /* send request */
+        $this->actingAs($user)->json('delete', '/api/checklists/' . $this->checklist->id . '/items/' . $item->id);
+
+        /* check status code */
+        $this->seeStatusCode(204);
+    }
+}
